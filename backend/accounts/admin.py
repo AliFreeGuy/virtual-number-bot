91c0151816056990178aac62010161d4cc0184d0
AliFreeGuy <alifreeguy@gmail.com>
2024-07-14 17:41:09 +0330

    feat : add is_auth and auth_data in user model

--- backend/accounts/admin.py
+++ backend/accounts/admin.py
@@ -10,14 +10,14 @@ class CustomUserAdmin(UserAdmin):
     model = User
     add_form = CustomUserCreationForm
     form = CustomUserChangeForm
-    list_display = ('chat_id', 'full_name', 'is_admin', 'is_active', 'is_superuser', 'creation_shamsi', 'wallet')
-    list_filter = ('is_admin', 'is_active')
+    list_display = ('chat_id', 'full_name', 'is_admin', 'is_active' ,'is_auth' , 'auth_data', 'is_superuser', 'creation_shamsi', 'wallet')
+    list_filter = ('is_admin', 'is_active','is_auth' )
     search_fields = ('chat_id', 'full_name')
     ordering = ('-creation',)
 
     fieldsets = (
         (None, {'fields': ('chat_id', 'full_name', 'wallet' ,'password')}),
-        ('Permissions', {'fields': ('is_admin', 'is_active', 'is_superuser', 'groups',)}),
+        ('Permissions', {'fields': ('is_admin', 'is_active','is_auth' , 'auth_data', 'is_superuser', 'groups',)}),
     )
 
     search_fields = ['chat_id', 'full_name']
@@ -27,7 +27,7 @@ class CustomUserAdmin(UserAdmin):
     add_fieldsets = (
         (None, {
             'classes': ('wide',),
-            'fields': ('chat_id', 'full_name', 'password1', 'password2', 'is_admin', 'is_active', 'is_superuser' , 'wallet'),
+            'fields': ('chat_id', 'full_name', 'password1', 'password2', 'is_admin', 'is_active','is_auth' , 'auth_data' 'is_superuser' , 'wallet'),
         }),
     )
 
@@ -35,11 +35,7 @@ class CustomUserAdmin(UserAdmin):
         return jdatetime.datetime.fromgregorian(datetime=obj.creation).strftime('%Y/%m/%d %H:%M:%S')
     creation_shamsi.short_description = 'Creation'
 
-    def get_fieldsets(self, request, obj=None):
-        if obj and obj.is_admin:
-            return super().get_fieldsets(request, obj) + (('Permissions', {'fields': ('user_permissions',)}),)
-        return super().get_fieldsets(request, obj)
-
+    
     def get_readonly_fields(self, request, obj=None):
         if obj and not obj.is_admin:
             return super().get_readonly_fields(request, obj) + ('user_permissions',)
--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -11,9 +11,11 @@ class User(AbstractBaseUser , PermissionsMixin):
     full_name = models.CharField(max_length=128 , null=True ,blank=True)
     is_admin = models.BooleanField(default=False)
     is_active = models.BooleanField(default=True)
+    is_auth = models.BooleanField(default=False)
+    auth_data = models.CharField(default='no' , max_length=256)
     creation = models.DateTimeField(auto_now_add=True)
     wallet = models.PositiveBigIntegerField(default=0)
-   
+    
     USERNAME_FIELD = 'chat_id'
     REQUIRED_FIELDS = ['full_name' , ]
 
