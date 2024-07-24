a7674c7c10f8726e8c7aea39cf4af6107ba53169
AliFreeGuy <alifreeguy@gmail.com>
2024-07-24 12:46:16 +0330

    feat : add export-session-string.py

--- export_session_string.py
+++ export_session_string.py
@@ -11,7 +11,7 @@ API_HASH = env.get('API_HASH')
 BOT_TOKEN = env.get('BOT_TOKEN')
 
 
-bot = Client('test' , api_id=API_ID , api_hash=API_HASH , bot_token=BOT_TOKEN , proxy=PROXY)
+bot = Client('test' , api_id=API_ID , api_hash=API_HASH , proxy=PROXY)
 
 with bot :
     data = bot.export_session_string()
